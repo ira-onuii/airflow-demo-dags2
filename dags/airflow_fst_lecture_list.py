71c4f9aa07fc38464aa6361f07820f1aa7357666
chad <chad@onuii.com>
2024-11-13 18:03:25 +0900

    added fst_lecture dag

--- dags/airflow_fst_lecture_list.py
+++ dags/airflow_fst_lecture_list.py
@@ -50,7 +50,7 @@ dag = DAG(
     'data-warehouse-test-tirno-AI',
     default_args=default_args,
     description='Run query and load result to S3',
-    start_date=datetime(2024, 11, 13, 17, 30),
+    start_date=datetime(2024, 11, 13, 6, 30),
     schedule='*/30 * * * *',
     catchup=False
 )
