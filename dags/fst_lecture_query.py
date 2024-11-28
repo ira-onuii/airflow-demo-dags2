83a970632b543c7d1a62cc00b758314609778355
chad <chad@onuii.com>
2024-11-28 18:22:01 +0900

    modified AI query

--- dags/fst_lecture_query.py
+++ dags/fst_lecture_query.py
@@ -11,8 +11,8 @@ with mlvt as (
 		, mlvt.teacher_user_No, mlvt.teacher_name, mlvt.matchedat, student_name
 		from 
 			(select row_number() over(partition by lectures[1][1] order by mlvt.matchedat asc) as rn, matchedat
-				, mlvt.lectures[1][1] as lecture_vt_No, mlvt.lectures[1][2][2] as student_name
-				, mlvt.matchedteacher[1] as teacher_user_No, mlvt.matchedteacher[2] as teacher_name
+				, mlvt.lectures[1][1] as lecture_vt_No
+				, mlvt.matchedteacher[1] as teacher_user_No, mlvt.matchedteacher[2] as teacher_name, mlvt.lectures[1][2][2] as student_name
 				from matching_mongodb.matching.matching_lvt mlvt
 				where mlvt.status = 'MATCHED'
 			) mlvt
@@ -26,21 +26,24 @@ with mlvt as (
 					from mysql.onuei.payment p
 				) p
 			where p.rn = 1
-			and p.payment_regdate >= cast('2024-11-15 00:00:00' as timestamp)
+			and p.payment_regdate >= cast('2024-11-01 00:00:00' as timestamp)
 		),
 	t as (
 		select t.user_No, t.seoltab_state_updateat, t.lecture_phone_number 
 			from mysql.onuei.teacher t
 			where t.seoltab_state = 'ACTIVE'
-			and t.seoltab_state_updateat >= cast('2024-11-15 00:00:00' as timestamp)
+			and t.seoltab_state_updateat >= cast('2024-11-01 00:00:00' as timestamp)
 		),
 	meta_data as (
-		select lvt.student_user_No ,lvt.lecture_vt_no, ttn.name as subject
+		select lvt.student_user_No ,lvt.lecture_vt_no, ttn.name as subject, th.tteok_ham_type, u.phone_number, lvt.tutoring_state, u.name as student_name
+			, lvt.reactive_datetime 
 			from mysql.onuei.lecture_video_tutoring lvt
+			inner join mysql.onuei."user" u on lvt.student_user_no = u.user_no 
+			inner join mysql.onuei.tteok_ham th on lvt.payment_item = th.tteok_ham_no 
 			inner join mysql.onuei.term_taxonomy_name ttn on lvt.lecture_subject_id = ttn.term_taxonomy_id 
-			where lvt.reactive_datetime is null
-			and lvt.tutoring_state = 'ACTIVE'
-		)
+			-- where lvt.reactive_datetime is null
+			-- and lvt.tutoring_state = 'ACTIVE'
+	)
 select mlvt.lecture_vt_No, md.subject, md.student_user_No, mlvt.student_name
 	, mlvt.teacher_user_No, mlvt.teacher_name, lvc.rn, lvc.page_call_room_id, lvc.tutoring_datetime
 	from mlvt
@@ -54,9 +57,9 @@ select mlvt.lecture_vt_No, md.subject, md.student_user_No, mlvt.student_name
             and lvs.tutoring_datetime >= cast('2024-11-15 00:00:00' as timestamp)
 		) lvc on (mlvt.lecture_vt_No = lvc.lecture_vt_no and mlvt.teacher_user_No = lvc.teacher_user_No)
 	inner join p on mlvt.lecture_vt_No = p.lecture_vt_no 
-	inner join meta_data md on md.lecture_vt_No = mlvt.lecture_vt_No
+	left join meta_data md on md.lecture_vt_No = mlvt.lecture_vt_No
 	inner join t on mlvt.teacher_user_No = t.user_no 
 	inner join mysql.onuei.lecture_teacher_vt ltvt on (mlvt.lecture_vt_No = ltvt.lecture_vt_no and mlvt.teacher_user_No = ltvt.teacher_user_no)
-	where ltvt.teacher_vt_status = 'ASSIGN'
-	order by mlvt.lecture_vt_No, lvc.rn 
+	-- where ltvt.teacher_vt_status = 'ASSIGN'
+	order by mlvt.lecture_vt_No, lvc.rn
 '''
