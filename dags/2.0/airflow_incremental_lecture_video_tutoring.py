72a200eaaf016c3f0dde20ec37b3999294cc20b7
chad <chad@onuii.com>
2024-12-17 17:34:43 +0900

    added incremental dag

--- dags/2.0/airflow_incremental_lecture_video_tutoring.py
+++ dags/2.0/airflow_incremental_lecture_video_tutoring.py
@@ -25,7 +25,7 @@ def save_to_s3_with_hook(data, bucket_name, folder_name, file_name, **kwargs):
     s3_key = f"{folder_name}/{file_name}" 
     data.to_csv(csv_buffer, index=False)
     hook = S3Hook(aws_conn_id='conn_S3')
-    hook.load_string(csv_buffer.getvalue(), s3_key=s3_key, bucket_name=bucket_name, replace=True)
+    hook.load_string(csv_buffer.getvalue(), key=s3_key, bucket_name=bucket_name, replace=True)
     kwargs['ti'].xcom_push(key='bucket_name', value=bucket_name)
     kwargs['ti'].xcom_push(key='s3_key', value=s3_key)
 
