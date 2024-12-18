0ad970120e191e1bb2244c28b0bc570712d488c9
chad <chad@onuii.com>
2024-12-18 15:38:45 +0900

    added incremental dag

--- dags/2.0/airflow_incremental_lecture_video_tutoring.py
+++ dags/2.0/airflow_incremental_lecture_video_tutoring.py
@@ -32,7 +32,7 @@ filename = 'pg_lecture_video_tutoring_'+date + '.csv'
 #     kwargs['ti'].xcom_push(key='s3_key', value=s3_key)
     
 
-
+# S3 버킷 및 디렉토리 지정
 def save_to_s3_with_hook(data, bucket_name, folder_name, file_name):
     csv_buffer = StringIO()
     data.to_csv(csv_buffer, index=False)
@@ -40,7 +40,7 @@ def save_to_s3_with_hook(data, bucket_name, folder_name, file_name):
     hook.load_string(csv_buffer.getvalue(), key=f"{folder_name}/{file_name}", bucket_name=bucket_name, replace=True)
 
 
-
+# incremental_extract 결과 받아와서 S3에 저장
 def save_results_to_s3(**context):
     query_results = context['ti'].xcom_pull(task_ids='incremental_extract_and_load')
     column_names = ['lecture_vt_no','student_user_no','lecture_subject_id','student_type','tutoring_state','payment_item','next_payment_item','current_schedule_no','stage_max_cycle_count','stage_free_cycle_count','stage_pre_offer_cycle_count','stage_offer_cycle_count','create_datetime','update_datetime','last_done_datetime','application_datetime','memo','total_subject_done_month','reactive_datetime']
@@ -53,28 +53,37 @@ def incremental_extract(**kwargs):
     from airflow.providers.postgres.hooks.postgres import PostgresHook
     from airflow.providers.mysql.hooks.mysql import MySqlHook
 
-    
+    # postgresql 연결
     pg_hook = PostgresHook(postgres_conn_id='postgres_dev_conn')  
+    # mysql 연결
     mysql_hook = MySqlHook(mysql_conn_id='legacy_staging_conn')   
 
     # SQLAlchemy Engine 생성
     pg_engine = pg_hook.get_sqlalchemy_engine()
     mysql_engine = mysql_hook.get_sqlalchemy_engine()
 
+    # 기존 data warehouse에 있던 데이터 추출 쿼리
     before_data = 'select * from raw_data.lecture_video_tutoring'
+
+    # 최근 실행시점 이후 update된 데이터 추출 쿼리
     today_data = warehouse_query.lvt_select_query
 
+    # 쿼리 실행 및 union all로 병합
     df_before = pd.read_sql(before_data, pg_engine)
     df_today = pd.read_sql(today_data, mysql_engine)
     df_union_all = pd.concat([df_before, df_today], ignore_index=True)
 
+    # date_type 변환
     df_union_all['update_datetime'] = pd.to_datetime(df_union_all['update_datetime'], errors='coerce')
 
-
+    # PK 값 별 최근 행이 1이 오도록 row_number 설정
     df_union_all['row_number'] = df_union_all.sort_values(by = ['update_datetime'], ascending = False).groupby(['lecture_vt_no']).cumcount()+1
-    df_union_all = df_union_all[['lecture_vt_no','student_user_no','lecture_subject_id','student_type','tutoring_state','payment_item','next_payment_item','current_schedule_no','stage_max_cycle_count','stage_free_cycle_count','stage_pre_offer_cycle_count','stage_offer_cycle_count','create_datetime','update_datetime','last_done_datetime','application_datetime','memo','total_subject_done_month','reactive_datetime']]
     
+    # PK 값 별 최근 행만 추출
     df_incremental = df_union_all[df_union_all['row_number'] == 1]
+    
+    # row_number 컬럼 제거 및 컬럼 순서 정렬
+    df_incremental = df_incremental[['lecture_vt_no','student_user_no','lecture_subject_id','student_type','tutoring_state','payment_item','next_payment_item','current_schedule_no','stage_max_cycle_count','stage_free_cycle_count','stage_pre_offer_cycle_count','stage_offer_cycle_count','create_datetime','update_datetime','last_done_datetime','application_datetime','memo','total_subject_done_month','reactive_datetime']]
 
     # 특정 컬럼만 NaN 처리 후 int로 변환
     df_incremental[['payment_item', 'next_payment_item', 'current_schedule_no']] = (
@@ -83,6 +92,7 @@ def incremental_extract(**kwargs):
         .astype('int64')  # 정수형으로 변환
     )
 
+    # 정제된 데이터 data_warehouse 테이블에 삽입
     df_incremental.to_sql(
         name='lecture_video_tutoring',  # 삽입할 테이블 이름
         con=pg_engine,  # PostgreSQL 연결 엔진
