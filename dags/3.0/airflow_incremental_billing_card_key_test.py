a973e72d739e6db9842591b07829055fc975884d
chad <chad@onuii.com>
2025-01-13 10:42:12 +0900

    modified 3.0 dags

--- dags/3.0/airflow_incremental_billing_card_key_test.py
+++ dags/3.0/airflow_incremental_billing_card_key_test.py
@@ -62,6 +62,7 @@ def incremental_extract():
     # 기존 data warehouse에 있던 데이터 추출 쿼리
     before_data = f'select max(id) from {pg_schema}."{trino_schema}.{table_name}"'
     before_data_result =  pd.read_sql(before_data, pg_engine)
+    print(before_data_result)
 
     # 최근 실행시점 이후 update된 데이터 추출 쿼리
     today_data = f'''
