a4e4e9eb6368496f9577270eea5f49a201100f79
chad <chad@onuii.com>
2025-01-13 15:36:12 +0900

    modified 3.0 dags

--- dags/3.0/airflow_incremental_active_user_benefit.py
+++ dags/3.0/airflow_incremental_active_user_benefit.py
@@ -69,9 +69,9 @@ def incremental_extract():
     max_updated_data_result =  pd.read_sql(max_updated_data, pg_engine)
     max_updatedat = max_updated_data_result['max_updatedat'].iloc[0]
     if max_updatedat is None:
-        max_updatedat = '2025-01-01 00:00:00'  # 기본값
+        max_updatedat = "cast('2025-01-01 00:00:00' as timestamp)"  # 기본값
     else:
-        max_updatedat
+        'cast(' + f"'{max_updatedat}'" + 'as timestamp)'
     print(max_updatedat)
 
     # 최근 실행시점 이후 update된 데이터 추출 쿼리
