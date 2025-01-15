3b639e24d4adf895f6912397d5784bf9087a451f
chad <chad@onuii.com>
2025-01-15 11:38:57 +0900

    modified 3.0 dags

--- dags/3.0/airflow_incremental_admin_operation.py
+++ dags/3.0/airflow_incremental_admin_operation.py
@@ -80,7 +80,7 @@ def incremental_extract():
     
     
     # row_number 컬럼 제거 및 컬럼 순서 정렬
-    df_incremental = df_today[["id","createdAt","adminUserId","name","departmentName","type","paymentId","blockRefundId"]]
+    df_incremental = df_today[["id","createdat","adminUserid","name","departmentname","type","paymentid","blockrefundid"]]
 
     
 
