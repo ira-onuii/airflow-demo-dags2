2d0942f49128f28cf8da1fb72211220e60159e68
chad <chad@onuii.com>
2025-01-23 17:43:16 +0900

    modified 3.0 dags

--- dags/3.0/demo_teacher_dashboard.py
+++ dags/3.0/demo_teacher_dashboard.py
@@ -118,7 +118,7 @@ default_args = {
 }
 
 dag = DAG(
-    f'data-warehouse-test-postgresql-dashboard_demo-incremental_3.0',
+    f'data-warehouse-test-postgresql-teacher_dashboard_demo-incremental_3.0',
     default_args=default_args,
     description='Run query and load result to S3',
     schedule='10 17 * * *',
