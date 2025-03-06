406aad7112f8a5df778eee475970ae7ebdeacde7
chad <chad@onuii.com>
2025-03-06 15:37:45 +0900

    modified 3.0 dags

--- dags/3.0/airflow_incremental_first_extract.py
+++ dags/3.0/airflow_incremental_first_extract.py
@@ -145,7 +145,7 @@ default_args = {
 }
 
 dag = DAG(
-    f'data-warehouse-test-postgresql-{table_name}-incremental_3.0',
+    f'data-warehouse-test-postgresql-first-extract-incremental_3.0',
     default_args=default_args,
     description='Run query and load result to S3',
     schedule='10 17 * * *',
