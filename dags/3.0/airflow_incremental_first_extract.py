95886deeacc1009f6b5c067caf421bd574f888ce
chad <chad@onuii.com>
2025-03-06 15:43:32 +0900

    modified 3.0 dags

--- dags/3.0/airflow_incremental_first_extract.py
+++ dags/3.0/airflow_incremental_first_extract.py
@@ -39,9 +39,9 @@ def save_to_s3_with_hook(data, bucket_name, version, folder_name, file_name):
 # incremental_extract 결과 받아와서 S3에 저장
 def save_results_to_s3(**context):
     query_results = context['ti'].xcom_pull(task_ids='incremental_extract_and_load')
-    column_names = ['idx','minutes_per_round','next_total_month','total_rounds_of_free','total_rounds_of_pay','used_rounds_of_free','used_rounds_of_pay','created_at','id','latest_round_id','lecture_id','updated_at','fixed_package_id','timeblock']
+    column_names = ["installment_period_of_next_payment","is_student","created_at","exit_date_time","id","join_date_time","latest_ticket_id","latest_ticket_wallet_id","latest_ticket_wallet_payment_id","lecture_id","updated_at","user_id"]
     df = pd.DataFrame(query_results, columns=column_names)
-    save_to_s3_with_hook(df, 'onuii-data-pipeline-3.0', 'staging',table_name, filename)
+    save_to_s3_with_hook(df, 'onuii-data-pipeline-3.0', 'live',table_name, filename)
 
 
 # 증분 추출 with row_number()
@@ -81,7 +81,7 @@ def incremental_extract():
     # 최근 실행시점 이후 update된 데이터 추출 쿼리
     today_data = f'''
        select 
-        "idx","minutes_per_round","next_total_month","total_rounds_of_free","total_rounds_of_pay","used_rounds_of_free","used_rounds_of_pay","created_at","id","latest_round_id","lecture_id","updated_at","fixed_package_id"
+        "installment_period_of_next_payment","is_student","created_at","exit_date_time","id","join_date_time","latest_ticket_id","latest_ticket_wallet_id","latest_ticket_wallet_payment_id","lecture_id","updated_at","user_id"
         from "{trino_database}"."{trino_schema}".{table_name}
     '''
 #where updated_at > cast('{max_updatedat}' as timestamp)
@@ -110,7 +110,7 @@ def incremental_extract():
     # print(df_incremental)
     
     # row_number 컬럼 제거 및 컬럼 순서 정렬
-    df_incremental = df_today[['idx','minutes_per_round','next_total_month','total_rounds_of_free','total_rounds_of_pay','used_rounds_of_free','used_rounds_of_pay','created_at','id','latest_round_id','lecture_id','updated_at','fixed_package_id']]
+    df_incremental = df_today[["installment_period_of_next_payment","is_student","created_at","exit_date_time","id","join_date_time","latest_ticket_id","latest_ticket_wallet_id","latest_ticket_wallet_payment_id","lecture_id","updated_at","user_id"]]
 
     # # 특정 컬럼만 NaN 처리 후 int로 변환
     # df_incremental[['payment_item', 'next_payment_item', 'current_schedule_no']] = (
