c71a98eb8e36108de1ccda63b5c581ba48ea8b9e
chad <chad@onuii.com>
2025-03-19 17:33:16 +0900

    modified 3.0 dbt dags

--- dbt/dbt_dev_3/models/3.0/done_month/RAW_DM/lecture_DM.sql
+++ dbt/dbt_dev_3/models/3.0/done_month/RAW_DM/lecture_DM.sql
@@ -2,14 +2,15 @@
 {{ config(
     materialized='table',
     schema='block_done-month',
-    unique_key='lecture_teacher_vt_No'
+    unique_key='lecture_id'
 ) }}
 
 
 WITH lecture_dm AS (
-   select user_id, lecture_id, "subject", lecture_status,sum(round_done_month) as lecture_done_month
-    from  {{ ref('round_DM') }}
-    group by user_id, lecture_id, lecture_status, "subject"
+   select user_id, lecture_id, "subject", l.status as lecture_status, sum(round_done_month) as lecture_done_month
+    from  {{ ref('round_DM') }} r
+    inner join raw_data."lecture_v2.lecture" l on r.lecture_id = l.id
+    group by user_id, lecture_id, l.status, "subject"
 )
 SELECT *
     from lecture_dm
