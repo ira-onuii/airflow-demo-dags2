028451b919e18f2ba9da9ee84971bb59fcc8536c
chad <chad@onuii.com>
2025-03-24 16:22:39 +0900

    modified 3.0 dbt dags

--- dags/2.0/airflow_fst_lecture_list_test.py
+++ dags/2.0/airflow_fst_lecture_list_test.py
@@ -55,7 +55,7 @@ default_args = {
 }
 
 dag = DAG(
-    'data-warehouse-test-tirno-AI_2.0_test',
+    'tirno-AI_2.0_test',
     default_args=default_args,
     description='Run query and load result to S3',
     start_date=datetime(2024, 11, 13, 6, 25),
