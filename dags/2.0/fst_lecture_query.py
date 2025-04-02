1eb9dfd22c2eb57bf3cd6f767101f98361e08ff8
chad <chad@onuii.com>
2025-04-02 15:37:21 +0900

    modified 2.0 AI lecture dag

--- dags/2.0/fst_lecture_query.py
+++ dags/2.0/fst_lecture_query.py
@@ -23,8 +23,8 @@ lvc_query_template = '''
 with lvc as (
 select lvc.lecture_cycle_No, lvc.lecture_vt_no, lvc.page_call_room_id
 	from mysql.onuei.lecture_vt_cycles lvc
-    where lvs.req_datetime >= timestamp '2025-03-27 00:00:00'
-    and lvs.req_datetime < timestamp '2025-04-02 11:24:59'
+    where lvc.req_datetime >= timestamp '2025-03-27 00:00:00'
+    and lvc.req_datetime < timestamp '2025-04-02 11:24:59'
     -- where lvc.req_datetime >= timestamp '{{ data_interval_start }}'
 	-- and lvc.req_datetime < timestamp '{{ data_interval_end }}'
 )
