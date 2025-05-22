cf4b46bdcd377f64ec55b20b6a2d17e9a6f1a422
chad <chad@onuii.com>
2025-05-22 17:50:09 +0900

    add CS dags

--- dags/2.0/operation/airflow_CS_application.py
+++ dags/2.0/operation/airflow_CS_application.py
@@ -8,37 +8,7 @@ from pendulum import timezone
 
 KST = timezone("Asia/Seoul")
 
-query = '''
-with lvt as (
-select lvt.lecture_vt_no,lvt.student_user_no, lvt.tutoring_state,ttn.name as subject, u.name as student_name, if(lvt.reactive_datetime is null,lvt.create_datetime, reactive_datetime) as crda
-	from mysql.onuei.lecture_video_tutoring lvt
-	inner join mysql.onuei."user" u on lvt.student_user_No = u.user_no 
-	inner join mysql.onuei.term_taxonomy_name ttn on lvt.lecture_subject_id = ttn.term_taxonomy_id 
-	where u.email_id not like '%onuii%'
-	and u.email_id not like '%test%'
-	and u.phone_number not like '%00000%'
-	and u.phone_number not like '%11111%'
-	and u.name not like '%테스트%'
-    and u.user_No not in (621888,621889,622732,615701)
-	and lvt.student_type in ('PAYED','PAYED_B')
-	and lvt.tutoring_state = 'REGISTER'
-	and lvt.application_datetime is null
-),
-p as (
-select p.lecture_vt_no, p.student_user_No, p.student_name, p.subject, p.state, p.payment_regdate
-	from 
-		(select row_number() over(partition by p.lecture_vt_no order by p.payment_regdate asc) as rn 
-			,p.payment_regdate, p.state, lvt.*
-			from mysql.onuei.payment p
-			inner join lvt on (p.lecture_vt_no = lvt.lecture_vt_no and p.payment_regdate >= lvt.crda)
-		) p
-	where p.rn = 1
-	and p.state = '결제완료'
-	and date_diff('day', p.payment_regdate, now()) >= 3
-)
-select p.student_name, p.student_user_No, p.subject
-	from p
-'''
+
 
 # 구글 인증 설정
 def authorize_gspread():
@@ -98,8 +68,48 @@ def update_google_sheet_append_by_column(range_start_cell, dataframe):
     sheet.update(range_start_cell, values)
 
 
+# def update_google_sheet(range_start_cell, dataframe):
+#     sheet = google_conn(sheet_name='과외신청서 미작성 CS 확인용')
+
+    
+#     # Pandas DF를 시트에 쓰기 위해 리스트 변환
+#     values = [dataframe.columns.tolist()] + dataframe.values.tolist()
+#     sheet.update(range_start_cell, values)
+
+
 def run_query():
     from airflow.providers.trino.hooks.trino import TrinoHook
+    query = '''
+with lvt as (
+select lvt.lecture_vt_no,lvt.student_user_no, lvt.tutoring_state,ttn.name as subject, u.name as student_name, if(lvt.reactive_datetime is null,lvt.create_datetime, reactive_datetime) as crda
+	from mysql.onuei.lecture_video_tutoring lvt
+	inner join mysql.onuei."user" u on lvt.student_user_No = u.user_no 
+	inner join mysql.onuei.term_taxonomy_name ttn on lvt.lecture_subject_id = ttn.term_taxonomy_id 
+	where u.email_id not like '%onuii%'
+	and u.email_id not like '%test%'
+	and u.phone_number not like '%00000%'
+	and u.phone_number not like '%11111%'
+	and u.name not like '%테스트%'
+    and u.user_No not in (621888,621889,622732,615701)
+	and lvt.student_type in ('PAYED','PAYED_B')
+	and lvt.tutoring_state = 'REGISTER'
+	and lvt.application_datetime is null
+),
+p as (
+select p.lecture_vt_no, p.student_user_No, p.student_name, p.subject, p.state, p.payment_regdate
+	from 
+		(select row_number() over(partition by p.lecture_vt_no order by p.payment_regdate asc) as rn 
+			,p.payment_regdate, p.state, lvt.*
+			from mysql.onuei.payment p
+			inner join lvt on (p.lecture_vt_no = lvt.lecture_vt_no and p.payment_regdate >= lvt.crda)
+		) p
+	where p.rn = 1
+	and p.state = '결제완료'
+	and date_diff('day', p.payment_regdate, now()) >= 3
+)
+select p.student_name, p.student_user_No, p.subject
+	from p
+'''
     trino_hook = TrinoHook(trino_conn_id='trino_conn')   
 
     # SQLAlchemy Engine 생성
