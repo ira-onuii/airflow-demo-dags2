c3c03627c0d4be4a1042e6af05770bcb59c91659
chad <chad@onuii.com>
2025-05-22 16:55:52 +0900

    add CS dags

--- dags/2.0/operation/airflow_CS_application.py
+++ dags/2.0/operation/airflow_CS_application.py
@@ -59,13 +59,39 @@ def google_conn(sheet_name):
     return sheet
 
 # 쿼리 결과를 시트에 업로드
-def update_google_sheet(range_start_cell, dataframe):
-    sheet = google_conn(sheet_name='과외신청서 미작성 CS 확인용')
+# def update_google_sheet(range_start_cell, dataframe):
+#     sheet = google_conn(sheet_name='과외신청서 미작성 CS 확인용')
 
     
-    # Pandas DF를 시트에 쓰기 위해 리스트 변환
+#     # Pandas DF를 시트에 쓰기 위해 리스트 변환
+#     values = [dataframe.columns.tolist()] + dataframe.values.tolist()
+#     sheet.update(range_start_cell, values)
+
+# 쿼리 결과 append
+def update_google_sheet_append_by_column(dataframe: pd.DataFrame, sheet_name: str):
+    sheet = google_conn(sheet_name=sheet_name)
+    
+    # 대상 열 전체 읽기 (예: 'A2:A')
+    col_range = "B2:D"
+    col_values = sheet.get(col_range)
+    
+    # 빈 행 찾기: 값이 없으면 빈 리스트거나 빈 문자열
+    first_empty_row = 2  # A2부터 시작
+    for i, row in enumerate(col_values, start=2):
+        if not row or not row[0]:  # 빈 값이면
+            first_empty_row = i
+            break
+    else:
+        # 모두 채워졌다면 그 아래 행부터 입력
+        first_empty_row = len(col_values) + 2
+
+    # 업데이트할 셀 범위
+    range_start_cell = f"B{first_empty_row}"
+    
+    # 데이터프레임을 시트에 쓰기 위한 리스트로 변환
     values = [dataframe.columns.tolist()] + dataframe.values.tolist()
     sheet.update(range_start_cell, values)
+
 
 def run_query():
     from airflow.providers.trino.hooks.trino import TrinoHook
@@ -74,7 +100,7 @@ def run_query():
     # SQLAlchemy Engine 생성
     trino_engine = trino_hook.get_sqlalchemy_engine()
     CS_list = pd.read_sql(query, trino_engine)
-    df = CS_list
+    df = CS_list.values.tolist()
     return df
 
 # DAG 내에서 사용될 함수
@@ -82,7 +108,7 @@ def upload_daily_data():
     # Trino 연결
     # 실제 쿼리 → Pandas DF 처리
     df = run_query()
-    update_google_sheet(range_start_cell='B365', dataframe=df)
+    update_google_sheet_append_by_column(dataframe=df, sheet_name='과외신청서 미작성 CS 확인용')
 
 
 # DAG 정의
