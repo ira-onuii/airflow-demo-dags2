d105e7a81de9a63fdce16f49dc23e348b252f0c7
chad <chad@onuii.com>
2025-06-19 16:57:20 +0900

    modified round_done_month dbt model

--- dbt/dbt_dev_2/models/2.0/done_month/RAW_DM/round_done_month.sql
+++ dbt/dbt_dev_2/models/2.0/done_month/RAW_DM/round_done_month.sql
@@ -9,7 +9,7 @@ WITH update_existing AS (
     SELECT 
         lvs.schedule_No, lvs.lecture_cycle_no, lvs.stage_count, lvs.cycle_count,
         lvs.update_datetime, cycle_payment_item,
-        lvs.lecture_vt_No, sf.student_user_No, sf.teacher_user_no, lvs.per_done_month,
+        lvs.lecture_vt_No, sf.student_user_No, sf.teacher_user_no, round(lvs.per_done_month,3) as per_done_month,
         now() as created_at
     FROM raw_data.lecture_vt_schedules lvs
     INNER JOIN raw_data.student_follow sf ON lvs.follow_no = sf.follow_no
