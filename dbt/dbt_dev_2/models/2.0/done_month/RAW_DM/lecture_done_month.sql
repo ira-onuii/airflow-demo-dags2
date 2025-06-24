2f8a6d1efc40195cb0111354226e41674fc09dfe
chad <chad@onuii.com>
2025-06-24 11:06:29 +0900

    add lecture_done_month model & dag

--- dbt/dbt_dev_2/models/2.0/done_month/RAW_DM/lecture_done_month.sql
+++ dbt/dbt_dev_2/models/2.0/done_month/RAW_DM/lecture_done_month.sql
@@ -1,24 +1,25 @@
 {{ config(
     materialized='incremental',
+    unique_key='student_teacher_no',
     schema='block_done-month',
-    incremental_strategy='merge',
-    unique_key='lecture_teacher_vt_No'
+    on_schema_change='append_new_columns',
+    incremental_strategy='merge'
 ) }}
 
-
-WITH lecture_dm AS (
-    select ltvt.lecture_teacher_vt_No, lvt.lecture_vt_no, lvt.student_user_No, lvt.tutoring_state, ltvt.teacher_user_no, ltvt.active_done_month, ltvt.total_done_month
-        ,ltvt.create_at, ltvt.update_at 
-		from raw_data.lecture_teacher_vt ltvt
-		inner join raw_data.lecture_video_tutoring lvt on ltvt.lecture_vt_no = lvt.lecture_vt_no 
-		-- where lvts.per_done_month is not null 
+WITH update_existing AS (
+    SELECT
+        concat(rdm.student_user_no, rdm.teacher_user_no) AS student_teacher_no,
+        rdm.student_user_no,
+        rdm.teacher_user_no,
+        SUM(per_done_month) AS lecture_done_month,
+        MAX(update_datetime) AS max_update_datetime
+    FROM {{ ref('round_done_month') }} rdm
+    GROUP BY rdm.student_user_no, rdm.teacher_user_no
 )
-SELECT lecture_teacher_vt_No, lecture_vt_no, student_user_No, tutoring_state, teacher_user_no, active_done_month, total_done_month
-    , {{ get_updated_at('ld', this, 'lecture_teacher_vt_No', 'now()') }} as updated_at
-    FROM lecture_dm ld
+
+SELECT *
+FROM update_existing
 
 {% if is_incremental() %}
-
-  where ld.update_at > (select max(updated_at) from {{ this }})
-
-{% endif %}    
+WHERE max_update_datetime >= (SELECT MAX(max_update_datetime) FROM {{ this }})
+{% endif %}
