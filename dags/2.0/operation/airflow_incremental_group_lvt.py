97cd3f676959409d29971ffee6f2fb50dd4c84a8
chad <chad@onuii.com>
2025-07-01 17:28:05 +0900

    add group_lvt dag

--- dags/2.0/operation/airflow_incremental_group_lvt.py
+++ dags/2.0/operation/airflow_incremental_group_lvt.py
@@ -130,10 +130,7 @@ def incremental_extract():
     print(df_today)
 
     # 4. 병합 및 최신 row 추출
-    if df_before.empty:
-        df_union_all = df_today.copy()
-    else:
-        df_union_all = pd.concat([df_before, df_today], ignore_index=True)
+    df_union_all = pd.concat([df_before, df_today], ignore_index=True)
 
     df_union_all['row_number'] = df_union_all.sort_values(
         by=date_column, ascending=False
