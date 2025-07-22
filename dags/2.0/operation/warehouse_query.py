af532e96d4d158041a58ea48a6f92173fbe77c39
chad <chad@onuii.com>
2025-07-22 16:00:08 +0900

    modified group lvt query

--- dags/2.0/operation/warehouse_query.py
+++ dags/2.0/operation/warehouse_query.py
@@ -76,7 +76,7 @@ select list_4.lecture_vt_no, list_4.group_lecture_vt_no, list_4.active_timestamp
 	, cast(case when max(lvs.update_datetime) >= updated_at then max(lvs.update_datetime) else updated_at end as timestamp) as updated_at 
 	, min(tutoring_datetime) as min_tutoring_datetime, min(schedule_no) as min_schedule_no, sum(per_done_month) as done_month
 	from list_4
-	left join lvs on (list_4.lecture_vt_no = lvs.lecture_vt_no and list_4.active_timestamp <= lvs.tutoring_datetime and if(list_4.done_timestamp is null, now(), done_timestamp) >= lvs.tutoring_datetime)
+	left join lvs on (list_4.lecture_vt_no = lvs.lecture_vt_no and list_4.active_timestamp <= lvs.tutoring_datetime and if(list_4.done_timestamp is null, now() + interval '1' month, done_timestamp) >= lvs.tutoring_datetime)
 	where active_timestamp >= cast('2024-05-01' as timestamp)
 	group by list_4.lecture_vt_no, group_lecture_vt_no, active_timestamp, done_timestamp, min_payment_no, updated_at
 )
