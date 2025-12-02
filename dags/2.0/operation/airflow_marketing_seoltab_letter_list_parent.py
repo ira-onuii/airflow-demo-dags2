1a4599ea70b6c7a32ed6023de35823c364d1c30b
chad <chad@onuii.com>
2025-12-02 17:04:57 +0900

    add marketing dag

--- dags/2.0/operation/airflow_marketing_seoltab_letter_list_parent.py
+++ dags/2.0/operation/airflow_marketing_seoltab_letter_list_parent.py
@@ -9,46 +9,6 @@ from pendulum import timezone
 KST = timezone("Asia/Seoul")
 
 
-active_student_query = '''
-    with list as (
-select lvt.student_user_no, u.phone_number, ttn.name as grade
-	from mysql.onuei.lecture_video_tutoring lvt 
-	inner join mysql.onuei."user" u on lvt.student_user_No = u.user_No 
-	inner join mysql.onuei.student s on student_user_No = s.user_No 
-	inner join mysql.onuei.term_taxonomy_name ttn on s."year" = ttn.term_taxonomy_id 
-	where student_type in ('PAYED','PAYED_B')
-	and tutoring_state not in ('FINISH','AUTO_FINISH','DONE')
-	and u.email_id not like '%test%'
-	and ttn.name not in ('N수생','초1','초2','초3','초4','초5','초6')
-	group by lvt.student_user_No, u.phone_number, ttn.name 
-)
-select student_user_no, phone_number, grade, now() + interval '9' hour as updated_at
-    from list
-'''
-
-inative_student_query = '''
--- 중단_학생
-with list as (
-select lvt.student_user_no, u.phone_number, ttn.name as grade, max(lvt.update_datetime) as max_done_time
-	from mysql.onuei.lecture_video_tutoring lvt 
-	inner join mysql.onuei."user" u on lvt.student_user_No = u.user_No 
-	inner join mysql.onuei.student s on student_user_No = s.user_No 
-	inner join mysql.onuei.term_taxonomy_name ttn on s."year" = ttn.term_taxonomy_id 
-	where student_type in ('PAYED','PAYED_B')
-	and tutoring_state in ('FINISH','AUTO_FINISH','DONE')
-	and u.email_id not like '%test%'
-	and ttn.name not in ('N수생','초1','초2','초3','초4','초5','초6')
-	group by lvt.student_user_No, u.phone_number, ttn.name 
-)
-select list.student_user_no, list.phone_number, list.grade, now() + interval '9' hour as updated_at
-	from list
-	where list.student_user_no not in (
-	select student_user_No 
-		from mysql.onuei.lecture_video_tutoring lvt 
-		where tutoring_state not in ('FINISH','AUTO_FINISH','DONE')
-	)
-	and max_done_time <= cast('2024-08-01 23:59:59' as timestamp)
-'''
 
 active_parent_query = '''
     with list as (
@@ -122,19 +82,6 @@ def clear_sheet(sheet_name):
 
 
 
-def update_google_sheet_active_student(dataframe):
-    df = dataframe.copy()
-    df = df.where(df.notnull(), "").astype(str) 
-    sheet = google_conn(sheet_name='학생_수강생')
-    sheet.batch_clear(["A2:D"])
-    sheet.update("A2:D", df.values.tolist())
-
-def update_google_sheet_inactive_student(dataframe):
-    df = dataframe.copy()
-    df = df.where(df.notnull(), "").astype(str) 
-    sheet = google_conn(sheet_name='학생_중단')
-    sheet.batch_clear(["A2:D"])
-    sheet.update("A2:D", df.values.tolist())
 
 def update_google_sheet_active_parent(dataframe):
     df = dataframe.copy()
@@ -151,22 +98,7 @@ def update_google_sheet_inactive_parent(dataframe):
     sheet.update("A2:D", df.values.tolist())
 
 
-def run_query_active_student():
-    from airflow.providers.trino.hooks.trino import TrinoHook
-    query = active_student_query
-    trino_hook = TrinoHook(trino_conn_id='trino_conn')
-    trino_engine = trino_hook.get_sqlalchemy_engine()
-    df = pd.read_sql(query, trino_engine)
-    return df
 
-
-def run_query_inactive_student():
-    from airflow.providers.trino.hooks.trino import TrinoHook
-    query = inative_student_query
-    trino_hook = TrinoHook(trino_conn_id='trino_conn')
-    trino_engine = trino_hook.get_sqlalchemy_engine()
-    df = pd.read_sql(query, trino_engine)
-    return df
 
 def run_query_active_parent():
     from airflow.providers.trino.hooks.trino import TrinoHook
@@ -186,11 +118,6 @@ def run_query_inactive_parent():
     return df
 
 
-def active_student_listup():
-    update_google_sheet_active_student(dataframe=run_query_active_student())
-
-def inactive_student_listup():
-    update_google_sheet_inactive_student(dataframe=run_query_inactive_student())
 
 def active_parent_listup():
     update_google_sheet_active_parent(dataframe=run_query_active_parent())
@@ -232,24 +159,12 @@ default_args = {
 with DAG(
     dag_id='marketing_seoltab_letter_google_sheet_update_dag',
     default_args=default_args,
-    schedule_interval='0 10 * * *',  # 매일 오전 10시
+    schedule_interval='0 6 * * 5',  # 매주 금요일 오전 6시
     catchup=False,
     tags=['2.0', 'operation', 'marketing'],
 ) as dag:
 
-    upload_active_student = PythonOperator(
-        task_id='upload_weekly_active_student_data',
-        python_callable=active_student_listup,
-        retries=5,
-        retry_delay=timedelta(seconds=2),
-    )
 
-    upload_inactive_student = PythonOperator(
-        task_id='upload_weekly_inactive_student_data',
-        python_callable=inactive_student_listup,
-        retries=5,
-        retry_delay=timedelta(seconds=2),
-    )
     upload_active_parent = PythonOperator(
         task_id='upload_weekly_active_parent_data',
         python_callable=active_parent_listup,
@@ -264,27 +179,6 @@ with DAG(
     )
 
 
-    upload_backup_active_student = PythonOperator(
-        task_id='upload_weekly_active_student_data_backup',
-        python_callable=upload_backup_table,
-        op_kwargs={
-            "sql": active_student_query,
-            "source": "active_student",
-        },
-        retries=5,
-        retry_delay=timedelta(seconds=2),
-    )
-
-    upload_backup_inactive_student = PythonOperator(
-        task_id='upload_weekly_inactive_student_data_backup',
-        python_callable=upload_backup_table,
-        op_kwargs={
-            "sql": inative_student_query,
-            "source": "inactive_student",
-        },
-        retries=5,
-        retry_delay=timedelta(seconds=2),
-    )
 
     upload_backup_active_parent = PythonOperator(
         task_id='upload_weekly_active_parent_data_backup',
@@ -313,4 +207,4 @@ with DAG(
 
 
 
-    upload_active_student >> upload_backup_active_student >> upload_inactive_student >> upload_backup_inactive_student >> upload_active_parent >> upload_backup_active_parent >> upload_inactive_parent >> upload_backup_inactive_parent
+upload_active_parent >> upload_backup_active_parent >> upload_inactive_parent >> upload_backup_inactive_parent
