d10018034ff9809873f8da1a1dd5f9b72d78bf2f
chad <chad@onuii.com>
2025-12-02 16:00:02 +0900

    add marketing dag

--- dags/2.0/operation/airflow_marketing_seoltab_letter_list.py
+++ dags/2.0/operation/airflow_marketing_seoltab_letter_list.py
@@ -202,12 +202,15 @@ def inactive_parent_listup():
 def upload_backup_table(sql : str):
     from airflow.providers.postgres.hooks.postgres import PostgresHook
     from sqlalchemy import text
+    from airflow.providers.trino.hooks.trino import TrinoHook
     pg_hook = PostgresHook(postgres_conn_id='postgres_marketing')  
     pg_engine = pg_hook.get_sqlalchemy_engine()
+    trino_hook = TrinoHook(trino_conn_id='trino_conn')
+    trino_engine = trino_hook.get_sqlalchemy_engine()
     
     print(f'=================={type(sql)}========================')
     print(sql)
-    df = pd.read_sql_query(sql=text(sql), con=pg_engine)
+    df = pd.read_sql_query(sql=text(sql), con=trino_engine)
     print(df)
     df.to_sql(
         name='seoltab_letter_backup',
